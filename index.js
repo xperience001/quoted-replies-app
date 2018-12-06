a198108d377f7bd6d3645784165051e3a65383df
Dara Oladosu <daradosu@gmail.com>
2018-12-06 09:11:20 +0100

    late first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+var Twitter = require('twitter');
+var options = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
+};
+
+var client = new Twitter(options);
+var stream = client.stream(
+  'statuses/filter',
+  {
+    track: '@quotedreplies please'
+  }
+);
+
+stream.on('data', function(tweet) {
+  var status = prepareStatus(tweet);
+  sendStatus(status, tweet.id_str);
+});
+
+stream.on('error', function(error) {
+  throw error;
+});
+
+function sendStatus(status, id) {
+  client.post('statuses/update',
+    {
+      status: status,
+      in_reply_to_status_id: id
+    }, function (err, data) {
+      if (err) {
+        throw error;
+      }
+  });
+}
+
+function prepareStatus(tweet) {
+  var staticUrlPartType1 = 'https://twitter.com/search?f=tweets&vertical=default&q=twitter.com';
+  var staticUrlPartType2 = 'https://twitter.com/search?f=tweets&vertical=default&q=https://twitter.com';
+  var usernameString = `Aye @${tweet.user.screen_name}! these are the links to the quoted replies you asked for`;
+  var dynamicUrlPart = `${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}`;
+  var searchLinkType1 = `${staticUrlType1}/${dynamicUrlPart}`;
+  var searchLinkType2 = `${staticUrlType2}/${dynamicUrlPart}`;
+  var status = `${usernameString} \n${searchLinkType1} \n${searchLinkType2} \n✨😊`;
+
+  return status;
+}
