4ece90c162366e5d9b4d53495c6a6878f1513556
Dara Oladosu <daradosu@gmail.com>
2019-03-30 17:43:52 +0100

    Shuffle start words

--- index.js
+++ index.js
@@ -42,9 +42,12 @@ function sendStatus(status, id) {
 }
 
 function prepareStatus(tweet) {
+  var startWords = ['Psst', 'Aye', 'Holla', 'Hey'];
+  var index = Math.floor(Math.random() * startWords.length);
+  var startWord = startWords[index];
   var staticUrlPartType1 = 'https://twitter.com/search?f=tweets&vertical=default&q=twitter.com';
   var staticUrlPartType2 = 'https://twitter.com/search?f=tweets&vertical=default&q=https://twitter.com';
-  var usernameString = `@${tweet.user.screen_name} Aye! these are the links to the quoted replies you asked for`;
+  var usernameString = `${startWord}! @${tweet.user.screen_name} these are the links to the quoted replies you asked for`;
   var dynamicUrlPart = `${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}`;
   var searchLinkType1 = `${staticUrlPartType1}/${dynamicUrlPart}`;
   var searchLinkType2 = `${staticUrlPartType2}/${dynamicUrlPart}`;
